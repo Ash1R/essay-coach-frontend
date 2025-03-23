556e2fbd6fa0c4f19158b83e846a1687f109bf2c
Ashir Rao <ar2558@cornell.edu>
2025-03-23 05:58:51 -0400

    new url

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -38,7 +38,7 @@ export default function Questions() {
       if (alreadyFollowedUp) return;
 
       try {
-        const res = await fetch('https://pleasant-rooster-illegally.ngrok-free.app/api/followup', {
+        const res = await fetch('https://5cc5-128-84-124-174.ngrok-free.app//api/followup', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
@@ -75,7 +75,7 @@ export default function Questions() {
       const reflections = { ...answers };
       delete reflections[0];
 
-      const res = await fetch('https://pleasant-rooster-illegally.ngrok-free.app/api/suggestions', {
+      const res = await fetch('https://5cc5-128-84-124-174.ngrok-free.app//api/suggestions', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
